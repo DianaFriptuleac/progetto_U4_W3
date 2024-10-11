f82c30b90b260c8b823b94df64670f2d2cde0a75
Diana <dianadorojuc3@gmail.com>
2024-10-11 10:39:51 +0200

    Initial commit

---
+++ src/main/java/Diana_Friptuleac/Application.java
@@ -0,0 +1,8 @@
+package Diana_Friptuleac;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/enums/GenereLibri.java
@@ -0,0 +1,5 @@
+package enums;
+
+public enum GenereLibri {
+    BIOGRAFIA, STORICO, GIALLO, FANTASY, ROMANZI
+}
